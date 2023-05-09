529315b3b9973ec82853eaa2ac2003df23cb3b1a
형종 <127183087+brother-jong@users.noreply.github.com>
2023-05-09 11:19:46 +0900

    Add files via upload

--- Chapter05/5장_0503_3_변형종.py
+++ Chapter05/5장_0503_3_변형종.py
@@ -6,20 +6,6 @@
 설명 : 1부터 1000까지의 정수 중 사용자가 입력한 숫자의 배수만을 더하여 출력하는 프로그램을 작성하시오. while, continue 사용
 '''
 
-number = int(input("숫자를 입력하세요: "))
-total = 0
-i = 1
-
-while i <= 1000:
-    if i % number != 0:   # 입력한 숫자의 배수가 아니면 건너뜁니다.
-        i += 1
-        continue
-    total += i   # 입력한 숫자의 배수인 경우에만 더합니다.
-    i += 1
-
-print("1부터 1000까지의 정수 중 {}의 배수의 합은 {}입니다.".format(number, total))
-
-
 # 1. 정수를 입력받는다. (배수 - input_num)
 # 2. 입력받은 정수가 1부터 (num)
 # 3. 합계는 0
@@ -34,3 +20,43 @@ print("1부터 1000까지의 정수 중 {}의 배수의 합은 {}입니다.".for
 #       - 다시 처음으로 돌아간다. continue
 #   e 수를 1 증가시킨다.
 # 5. 합계를 출력한다.
+
+input_num = int(input("정수를 입력하세요: "))
+num = 0
+sum = 0
+while num <= 10 :
+    num = num + 1 # 증감식
+    if num % input_num == 0 :
+        sum = sum + num
+    else :
+        continue
+print("{}의 배수의 합 : {}" .format(input_num, sum))
+
+print("-----------------------------------------------------------------------------")
+
+input_num = int(input("정수를 입력하세요: "))
+num = 1
+sum = 0
+while num <= 10 :
+    if num % input_num == 0 :
+        sum = sum + num
+        num = num + 1 # 증감식
+    else :
+        num = num + 1 # 증감식
+        continue
+print("{}의 배수의 합 : {}" .format(input_num, sum))
+
+print("---for 반목문-----------------------------------------------------------------------------")
+
+input_num = int(input("정수를 입력하세요: "))
+
+sum = 0
+
+for num in range(1, 11) : # 1부터 10까지 반복
+    if num % input_num == 0 : # 0이면
+        sum = sum + num # 합해라
+    else : # 나머지가 0이면
+        continue # 다시 돌아감
+        
+
+print("{}의 배수의 합 : {}" .format(input_num, sum))
